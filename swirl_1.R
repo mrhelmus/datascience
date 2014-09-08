054e76dd1b1001ac6bc3f19d7125dab1dc8f5f74
mrhelmus <mrhelmus@gmail.com>
2014-09-08 11:39:48 -0400

    added

--- swirl_1.R
+++ swirl_1.R
@@ -9,8 +9,55 @@ library(swirl)
 # | -- Typing info() displays these options again.
 # 
 
-install_from_swirl("Getting and Cleaning Data")
+#install_from_swirl("Getting and Cleaning Data")
 swirl()
 
-mydf<-read.csv()
+mydf<-read.csv(path2csv,stringsAsFactors=FALSE)
+dim(mydf)
+head(mydf)
+library(dplyr)
+packageVersion("dplyr")
+cran<-tbl_df(mydf)
+rm("mydf")
+
+#select
+
+select(cran,ip_id,package, country)  #selcts particular columns in the order specified
+select(cran,r_arch:country)          #selects columns between the two column headers
+select(cran, -time)                  #removes a particular column
+select(cran,-(X:size))
+
+#filter
+
+filter(cran,package == "swirl")      #selects particular rows with a particular value
+filter(cran,r_version=="3.1.1",country=="US")
+filter(cran,r_version <="3.0.2",country=="IN")
+filter(cran,country=="US"|country=="IN")
+filter(cran,size > 100500,r_os == "linux-gnu")
+filter(cran,!is.na(r_version ))                   #remove all rows with an NA in a particular column
+
+#arrange
+
+cran2<-select(cran,size:ip_id)
+arrange(cran2,ip_id)              #order by ascending
+arrange(cran2,desc(ip_id))              #order by descending
+arrange(cran2,package,ip_id)              #order by multiple variables
+arrange(cran2,country,desc(r_version),ip_id)              #order by multiple variables
+
+#mutate
+
+cran3<-select(cran,ip_id,package,size)      #adds columns
+mutate(cran3,size_mb=size/2^20)
+mutate(cran3,size_mb=size/2^20,size_gb=size_mb/2^10)
+mutate(cran3,correct_size=size+1000)
+
+#summarize
+
+summarize(cran,avg_bytes=mean(size))
+
+
+
+
+
+
 
